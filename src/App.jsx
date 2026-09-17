eef8d9093e02e8458c02f0aed1c06c8495158bcb
Achehri-Hassan <hachehri@gmail.com>
2026-09-17 18:50:03 +0200

    chore: setup React and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+import React from 'react'
+
+function App() {
+  return (
+      <h1 className="text-5xl font-bold text-red-500">
+      Pizza Website 🍕
+    </h1>
+  )
+}
+
+export default App
